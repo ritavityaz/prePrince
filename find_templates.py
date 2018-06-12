3e488c771b52b30a7936935d681859bb663483d8
ritavityaz <margaryta.vityaz@gmail.com>
2018-06-11 23:42:55 -0700

    Cleaned up find_templates

--- find_templates.py
+++ find_templates.py
@@ -1,89 +1,76 @@
 from Bio import SeqIO
 
 
-templates = list(SeqIO.parse("amplicons.fasta", "fasta"))
-pre_templates = [str(t.seq) for t in templates]
-template_id = [str(t.id) for t in templates]
+##amplicons = amplicons.fasta"
+def template_search(amplicons):
 
-print(pre_templates[1])
+    #Extract sequences and such
+    templates = list(SeqIO.parse(amplicons, "fasta"))
+    pre_templates = [str(t.seq) for t in templates]
+    template_id = [str(t.id) for t in templates]
 
-# k = 77
-
-# min_score = int(k)
-
-
-# kmers = [pre_templates[0][i:i + k] for i in range(0, len(pre_templates[0]) - k + 1)]
-
-# scores = []
-file = open("found_templates.fasta","w+")
-for index,t in enumerate(pre_templates):
-    file.write(">" + (template_id[index])[:-6] + "\n")
+    # k = 77
+    # min_score = int(k)
+    # kmers = [pre_templates[0][i:i + k] for i in range(0, len(pre_templates[0]) - k + 1)]
+    # scores = []
 
 
-    # print((template_id[index])[:-6])
+    file = open("found_templates_" + str(amplicons),"w+")
+    for index, t in enumerate(pre_templates):
 
-    candidates = {}
-    for k in range(3, len(pre_templates[index])):
-        min_score = int(k * 0.75)
-        kmers = [pre_templates[index][i:i + k] for i in range(0, len(pre_templates[index]) - k + 1)]
-        scores = []
-        repeats = []
+        file.write(">" + (template_id[index])[:-6] + "\n")
 
-        for kmer_index in range(len(kmers) - k):
-            listOfSplits = [pre_templates[index][i:i + k] for i in range(kmer_index, len(pre_templates[index]), k)]
+        candidates = {}
 
+        for k in range(3, len(pre_templates[index])):
+            min_score = int(k * 0.75)
+            kmers = [pre_templates[index][i:i + k] for i in range(0, len(pre_templates[index]) - k + 1)]
+            scores = []
+            repeats = []
 
-            #Get rid on incomplete splits
-            if len(listOfSplits[-1]) != len(listOfSplits[0]):
-                listOfSplits = listOfSplits[0:len(listOfSplits)-1]
+            for kmer_index in range(len(kmers) - k):
+                listOfSplits = [pre_templates[index][i:i + k] for i in range(kmer_index, len(pre_templates[index]), k)]
 
-            #Get rid of first split(it's the same as the k-mer)
-            listOfSplits = listOfSplits[1:]
+                #Get rid on incomplete splits
+                if len(listOfSplits[-1]) != len(listOfSplits[0]):
+                    listOfSplits = listOfSplits[0:len(listOfSplits)-1]
 
-
-            idx = 0
-
-            kmer_score = 0
-            number_of_repeats = 0
-
-            for split in listOfSplits:
-                # print(idx)
-                idx += 1
-                score = sum(el1 == el2 for el1, el2 in zip(kmers[kmer_index], split))
-                # print(score)
-
-                if score < min_score:
-                    # print("broke")
-                    break
-
-                if score >= min_score:
-                    kmer_score += score
-                    number_of_repeats += 1
-                    # print(kmer_score)
-                    # print(kmers[kmer_index])
-
-            scores.append(kmer_score)
-            repeats.append(number_of_repeats)
-
-        if len(scores) == 0:
-            max_score = 0
-
-        if len(scores) > 0:
-            max_score = max(scores)
-
-            if max_score != 0:
-                idx_max_score = scores.index(max_score)
-                candidates[kmers[idx_max_score]] = max_score
+                #Get rid of first split(it's the same as the k-mer)
+                listOfSplits = listOfSplits[1:]
 
 
-        # print(repeats)
-        # print(max_score)
+                idx = 0
+                kmer_score = 0
+                number_of_repeats = 0
 
-        # print(kmers[idx_max_score])
+                for split in listOfSplits:
+                    idx += 1
+                    score = sum(el1 == el2 for el1, el2 in zip(kmers[kmer_index], split))
+
+                    if score < min_score:
+                        break
+
+                    if score >= min_score:
+                        kmer_score += score
+                        number_of_repeats += 1
+
+                scores.append(kmer_score)
+                repeats.append(number_of_repeats)
+
+            if len(scores) == 0:
+                max_score = 0
+
+            if len(scores) > 0:
+                max_score = max(scores)
+
+                if max_score != 0:
+                    idx_max_score = scores.index(max_score)
+                    candidates[kmers[idx_max_score]] = max_score
+
+        # print(candidates)
+        file.write(max(candidates, key=candidates.get) + "\n")
+        # print(max(candidates, key=candidates.get))
+    file.close()
 
 
-    # print(candidates)
-    file.write(max(candidates, key=candidates.get) + "\n")
-    # print(max(candidates, key=candidates.get))
-
-file.close()
+# template_search("kurono_amplicons.fasta")
